85f665dcdfc29756c5afc1b0a2619a74324671bc
shadi <shadi.zold@hotmail.com>
2022-02-12 09:33:17 -0800

    Formatted files

--- Q4/Queue.cpp
+++ Q4/Queue.cpp
@@ -1,4 +1,4 @@
-/* 
+/*
  * Queue.cpp
  *
  * Description: Implementation of an int sequence with enqueue/dequeue ...
@@ -7,43 +7,39 @@
  * Author:
  * Date:
  */
- 
+
 #include "Queue.h"
 
-
 // Description:  Constructor
-Queue::Queue() : elementCount(0), capacity(INITIAL_CAPACITY), frontindex(0), backindex(0) {
-} 
-
+Queue::Queue() : elementCount(0), capacity(INITIAL_CAPACITY), frontindex(0), backindex(0)
+{
+}
 
 // Description:  Inserts element x at the back (O(1))
-void Queue::enqueue(int x) {
+void Queue::enqueue(int x)
+{
     elementCount++;
     elements[backindex] = x;
     backindex = (backindex + 1) % capacity;
-} 
-
+}
 
 // Description:  Removes the frontmost element (O(1))
 // Precondition:  Queue not empty
-void Queue::dequeue() {
+void Queue::dequeue()
+{
     elementCount--;
     frontindex = (frontindex + 1) % capacity;
-} 
-
+}
 
 // Description:  Returns a copy of the frontmost element (O(1))
 // Precondition:  Queue not empty
-int Queue::peek() const {
+int Queue::peek() const
+{
     return elements[frontindex];
-} 
-
-
-// Description:  Returns true if and only if queue empty (O(1))
-bool Queue::isEmpty() const {
-    return elementCount == 0;
 }
 
-
-
-
+// Description:  Returns true if and only if queue empty (O(1))
+bool Queue::isEmpty() const
+{
+    return elementCount == 0;
+}
--- Q4/main.cpp
+++ Q4/main.cpp
@@ -3,34 +3,35 @@
 #include <iostream>
 using namespace std;
 
-
-int main () {
+int main()
+{
     Queue Q;
 
     // enqueue 1, 2, 3, 4, 5
-    for (int i = 1; i <= 5; i++) {
+    for (int i = 1; i <= 5; i++)
+    {
         Q.enqueue(i);
         cout << "enqueue " << i << endl;
     }
 
-
     // dequeue 2x elements
-    for (int i = 0; i < 2; i++) {
+    for (int i = 0; i < 2; i++)
+    {
         int y = Q.peek();
         Q.dequeue();
         cout << "peek " << y << endl;
     }
 
-
     // enqueue 6, 7, 8, 9, 10
-    for (int i = 6; i <= 10; i++) {
+    for (int i = 6; i <= 10; i++)
+    {
         Q.enqueue(i);
         cout << "enqueue " << i << endl;
     }
 
-
     // dequeue all elements
-    while (!Q.isEmpty()) {
+    while (!Q.isEmpty())
+    {
         int y = Q.peek();
         Q.dequeue();
         cout << "peek " << y << endl;
@@ -38,4 +39,3 @@ int main () {
 
     return 0;
 }
-
