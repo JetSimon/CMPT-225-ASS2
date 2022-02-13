ebfe693d595792fbe8e7bf1d9fe9407c0dcda723
shadi <shadi.zold@hotmail.com>
2022-02-13 09:01:45 -0800

    Finished q3, added sample tests. code runs as intended

--- Q3/Eval.cpp
+++ Q3/Eval.cpp
@@ -1,3 +1,16 @@
+/* 
+ * Eval.cpp
+ *
+ * Description: Solves an Infix operation by utilizing two stacks;
+ *              number stack and operator stack
+ *
+ * Invariant: input is well-formed
+ *            negative numbers cannot be inputted (use arithmetic ex: to use -1 do (2-3))
+ *              
+ * Author: Shadi Zoldjalali and Jet Simon
+ * Date: 2/13/2022
+ */
+
 #include "Eval.h" // for operation function
 
 #include <iostream>
@@ -12,7 +25,7 @@ int main()
     t = S.getnext();
 
     // while T is not EOF or the operator stack is non empty
-    while (t.tt != eof || opstack.isEmpty() == false)
+    while (t.tt != eof || !opstack.isEmpty())
     {
         // if T is a number:
         if (t.tt == integer)
@@ -52,7 +65,7 @@ int main()
         else if (t.tt == pltok || t.tt == mitok || t.tt == eof)
         {
             // if the operator stack is nonempty and the top is one of +, -, *, /:
-            if (!opstack.isEmpty() && (opstack.peek().tt == pltok || opstack.peek().tt == mitok || opstack.peek().tt == slashtok))
+            if (!opstack.isEmpty() && (opstack.peek().tt == pltok || opstack.peek().tt == mitok || opstack.peek().tt == asttok || opstack.peek().tt == slashtok))
             {
                 // pop the top two numbers and the top operator
                 // perform operation
@@ -91,12 +104,14 @@ int main()
         }
     }
 
+    // calculate everything that's left in the stacks
     while (!opstack.isEmpty())
     {
         res = calc(numstack, opstack);
         numstack.push(res);
     }
 
+    // cout the answer
     cout << numstack.peek().val << endl;
 
     return 0;
